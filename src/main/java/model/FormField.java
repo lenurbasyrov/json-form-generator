b97a61409c28541f95025362ce15f1fbdf040bcb
lenur <lenur@example.com>
2026-05-07 22:44:53 +0200

    Added class FormField with variables

---
+++ src/main/java/model/FormField.java
@@ -0,0 +1,54 @@
+package main.java.model;
+
+import java.util.List;
+
+public class FormField {
+    private String id;
+    private String label;
+    private String controlType;
+    private String dataType;
+    private boolean required;
+    private List<String> options;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getControlType() {
+        return controlType;
+    }
+    public void setControlType(String controlType) {
+        this.controlType = controlType;
+    }
+
+    public String getDataType() {
+        return dataType;
+    }
+    public void setDataType(String dataType) {
+        this.dataType = dataType;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+}
