730a3d1fe56405f9e0a0cc6aa2f1bc0aa80dd65e
lenur <lenur@example.com>
2026-05-08 17:47:45 +0200

    Added class FieldComponentFactory to create the appropriate Swing component for each form field based on the `controlType`.

---
+++ src/main/java/factory/FieldComponentFactory.java
@@ -0,0 +1,46 @@
+package main.java.factory;
+
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
+
+import main.java.model.FormField;
+import main.java.ui.FieldComponentBinding;
+
+public class FieldComponentFactory {
+    public FieldComponentBinding create (FormField field) {
+        String type = field.getControlType() == null ? "textfield" : field.getControlType().toLowerCase();
+        JComponent component;
+
+        switch (type) {
+            case "textarea" -> {
+                JTextArea tArea = new JTextArea(4, 24);
+                tArea.setLineWrap(true);
+                tArea.setWrapStyleWord(true);
+                component = new JScrollPane(tArea);
+            }
+            case "dropdown" -> {
+                JComboBox<String> comboBox = new JComboBox<>();
+                if (field.getOptions() != null) {
+                    for (String option : field.getOptions()) {
+                        comboBox.addItem(option);
+                    }
+                }
+                component = comboBox;
+            }
+            case "checkbox" -> component = new JCheckBox();
+            case "spinner" -> component = new JSpinner(new SpinnerNumberModel(0, Integer.MIN_VALUE, Integer.MAX_VALUE,1));
+            case "datefield" ->{
+                JTextField tf = new JTextField();
+                tf.setToolTipText("YYYY-MM-DD");
+                component = tf;
+            }
+            default -> component = new JTextField();
+        }
+        return new FieldComponentBinding(field, component);
+    }
+}
